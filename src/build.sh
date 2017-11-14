9823179cbb0b54f81939fd757ec798386cf0583a
Alexandre Saumier Demers <asaumierdemers@gmail.com>
2017-11-14 16:51:10 -0500

    Move build script inside src, build new var font

--- src/build.sh
+++ src/build.sh
@@ -3,4 +3,4 @@ fontmake -o ttf-interpolatable -m RobotoDelta.designspace --no-production-names
 # build the variable font from the designspace
 fonttools varLib RobotoDelta.designspace
 # move the variable font to the fonts directory
-mv RobotoDelta-VF.ttf fonts/RobotoDelta-VF.ttf
+mv RobotoDelta-VF.ttf ../fonts/RobotoDelta-VF.ttf
