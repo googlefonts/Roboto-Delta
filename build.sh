a640831dfbc248b56d3336839ef642897648a55a
Alexandre Saumier Demers <asaumierdemers@gmail.com>
2017-11-14 16:43:23 -0500

    Add comments in build scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+# build the master_ttf_interpolatable from the master_ufo
 fontmake -o ttf-interpolatable -m RobotoDelta.designspace --no-production-names
+# build the variable font from the designspace
 fonttools varLib RobotoDelta.designspace
+# move the variable font to the fonts directory
 mv RobotoDelta-VF.ttf fonts/RobotoDelta-VF.ttf
