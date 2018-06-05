caa9a1f42962e0de8b497b25f439286d41b276fd
David Ross <david@djr.com>
2018-06-05 19:15:58 -0400

    wghtmin opszmax manual

--- scripts/makeInstance.py
+++ scripts/makeInstance.py
@@ -1,7 +1,7 @@
 from designSpaceDocument import DesignSpaceDocument, SourceDescriptor, InstanceDescriptor, AxisDescriptor
 from mutatorMath.ufo.document import DesignSpaceDocumentWriter, DesignSpaceDocumentReader
 
-path = u"/Users/david/Documents/Type_Network/Roboto-Delta/src/_RobotoDelta-newMasters.designspace"
+path = u"/Users/david/Documents/Typefaces/Venture/sources/MeritBadge_extra.designspace"
 doc = DesignSpaceDocumentReader(path, ufoVersion=2)
 doc.process(makeGlyphs=True, makeKerning=True, makeInfo=True)
 print 'done'
