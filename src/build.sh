9a839718213596571d2c855c3f4628b27dff56f4
CJ Dunn <cjdunn@Lappy-447.local>
2018-07-13 15:40:45 -0400

    build var font, clean up old designspace files

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,7 @@
 fontmake -o ttf-interpolatable -m _RobotoDelta-newMasters_min.designspace --no-production-names
 fonttools varLib _RobotoDelta-newMasters_min.designspace
+
+
+
+fontmake -o ttf-interpolatable -m _RobotoDelta-newMasters_min_7.designspace --no-production-names
+fonttools varLib _RobotoDelta-newMasters_min_7.designspace
