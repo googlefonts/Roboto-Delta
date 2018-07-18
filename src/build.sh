1502d0db45b06f66132994e51a03478ae08ab4da
CJ Dunn <cjdunn@Lappy-447.local>
2018-07-18 13:49:38 -0400

    cleanup

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,2 @@
-fontmake -o ttf-interpolatable -m _RobotoDelta-newMasters_min.designspace --no-production-names
-fonttools varLib _RobotoDelta-newMasters_min.designspace
-
-
-
-fontmake -o ttf-interpolatable -m _RobotoDelta-newMasters_min_7.designspace --no-production-names
-fonttools varLib _RobotoDelta-newMasters_min_7.designspace
+fontmake -o ttf-interpolatable -m RobotoDelta-clipped.designspace --no-production-names
+fonttools varLib RobotoDelta-clipped.designspace
