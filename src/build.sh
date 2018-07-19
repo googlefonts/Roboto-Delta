87ee1f8c426a85201991f58945178fe180ec1a68
CJ Dunn <cjdunn@Lappy-447.local>
2018-07-19 10:11:57 -0400

    update build script

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,6 @@
+fontmake -o ttf-interpolatable -m RobotoDelta.designspace --no-production-names
+fonttools varLib RobotoDelta.designspace
+
+
 fontmake -o ttf-interpolatable -m RobotoDelta-clipped.designspace --no-production-names
 fonttools varLib RobotoDelta-clipped.designspace
