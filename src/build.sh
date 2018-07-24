987ff213ad46bc62348b5cbcb040a5c0d7e830e6
CJ Dunn <cjdunn@Lappy-447.local>
2018-07-24 12:42:00 -0400

    try building simplified var fonts with just wght and with wght + wdth

--- src/build.sh
+++ src/build.sh
@@ -4,3 +4,7 @@ fonttools varLib RobotoDelta.designspace
 
 fontmake -o ttf-interpolatable -m RobotoDelta-clipped.designspace --no-production-names
 fonttools varLib RobotoDelta-clipped.designspace
+
+
+fontmake -o ttf-interpolatable -m RobotoDelta_v2.designspace --no-production-names
+fonttools varLib RobotoDelta_v2.designspace
