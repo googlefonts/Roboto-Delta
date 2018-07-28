649d71950f5e1d808e56e2c0d442094ef10048bb
David Ross <david@djr.com>
2018-07-28 18:19:36 -0400

    remove temp clipped ufos

--- scripts/makeInstance.py
+++
@@ -1,7 +0,0 @@
-from designSpaceDocument import DesignSpaceDocument, SourceDescriptor, InstanceDescriptor, AxisDescriptor
-from mutatorMath.ufo.document import DesignSpaceDocumentWriter, DesignSpaceDocumentReader
-
-path = u"/Users/david/Documents/Typefaces/Venture/sources/MeritBadge_extra.designspace"
-doc = DesignSpaceDocumentReader(path, ufoVersion=2)
-doc.process(makeGlyphs=True, makeKerning=True, makeInfo=True)
-print 'done'
